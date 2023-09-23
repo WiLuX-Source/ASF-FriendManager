7c6b23e1c0682fa6ee8ed9beb55fe4cc98ed2ee1
WiLuX <61833703+WiLuX-Source@users.noreply.github.com>
2023-09-23 19:28:11 +0300

    1.0.0.0

--- WiluxFriendManager/FriendManager.cs
+++ WiluxFriendManager/FriendManager.cs
@@ -8,9 +8,9 @@ namespace MyAwesomePlugin;
 
 #pragma warning disable CA1812 // ASF uses this class during runtime
 [UsedImplicitly]
-internal sealed class MyAwesomePlugin : IPlugin {
-	public string Name => nameof(MyAwesomePlugin);
-	public Version Version => typeof(MyAwesomePlugin).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
+internal sealed class WiluxFriendManager : IPlugin {
+	public string Name => nameof(WiluxFriendManager);
+	public Version Version => typeof(WiluxFriendManager).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
 
 	public Task OnLoaded() {
 		ASF.ArchiLogger.LogGenericInfo($"Hello {Name}!");
